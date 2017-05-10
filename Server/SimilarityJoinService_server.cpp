45a29a63c7ef1dc557057c20c5de6d6e1fb7adce
MengyuYao <mengycs@gmail.com>
2017-05-10 16:07:43 +0800

    update

--- Server/SimilarityJoinService_server.cpp
+++ Server/SimilarityJoinService_server.cpp
@@ -107,42 +107,38 @@ public:
 	}
 
 	void JoinByStrategy1(std::vector<int32_t> & _return, const std::vector<std::string> & Datas, const int32_t ThresholdK, const int32_t TimeOut) {
-		int dimension = joinEngine.uiFinanceDimension;
+		int dimension = joinEngine.uiDataDimension;
 		uint32_t querySize = Datas.size();
-		//if (querySize < 1)
-		//{
-		//	return;
-		//}
-		//double** buffer = new double*[querySize];
-		//for (int i = 0; i < querySize; i++)
-		//{
-		//	buffer[i] = new double[dimension];
-		//}
-		//
-		//vector<string> tmpVecStr;
-		//for (int i = 0; i < querySize; i++)
-		//{
-		//	tmpVecStr.clear();
-		//	joinEngine.splitString(Datas[i], tmpVecStr, " ");
-		//	memset(buffer[i], 0, sizeof(double)*dimension);
-		//	for (int j = 0; j < dimension&&j < tmpVecStr.size(); j++)
-		//	{
-		//		buffer[i][j] = stod(tmpVecStr[j]);
-		//	}
-		//}
-		//
-		//if (buffer[0][0] < 1 && buffer[1][1] < 1)
-		//{
-		//	joinEngine.normalize(buffer[0], dimension);
-		//}
+		if (querySize < 1)
+		{
+			return;
+		}
+		double** buffer = new double*[querySize];
+		for (int i = 0; i < querySize; i++)
+		{
+			buffer[i] = new double[dimension];
+		}
+		
+		vector<string> tmpVecStr;
+		for (int i = 0; i < querySize; i++)
+		{
+			tmpVecStr.clear();
+			joinEngine.splitString(Datas[i], tmpVecStr, " ");
+			memset(buffer[i], 0, sizeof(double)*dimension);
+			for (int j = 0; j < dimension&&j < tmpVecStr.size(); j++)
+			{
+				buffer[i][j] = stod(tmpVecStr[j]);
+			}
+			joinEngine.normalize(buffer[i], dimension);
+		}
 
-		_return = joinEngine.joinByStrategy1(NULL, querySize, ThresholdK);
-		/*
+		_return = joinEngine.joinByStrategy1(buffer, querySize, ThresholdK);
+
 		for (int i = 0; i < Datas.size(); i++)
 		{
 			delete[]buffer[i];
 		}
-		delete[] buffer;*/
+		delete[] buffer;
 	}
 
 	void JoinByStrategy2(std::vector<int32_t> & _return, const std::vector<std::string> & Datas, const int32_t ThresholdK, const int32_t TimeOut) {
